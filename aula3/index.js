c02cc6bae6519e551b8cd267908e71c1c5b8851b
Jonatas Santos Gandra <jonatassgandra@gmail.com>
2024-12-19 00:07:53 -0300

    I downloaded Nodemon.

--- aula3/index.js
+++ aula3/index.js
@@ -4,20 +4,20 @@ const porta = 3000;
 const hostname = '192.168.15.51'
 app.get('/', (req, res) => {
 
-    res.send('pagina inicial');
+    res.send('Página inicial');
 });
 app.get('/sobre-empresa', (req, res) => {
 
-    res.send('pagina sobre a empresa');
+    res.send('Página sobre a empresa');
 });
 app.get('/contato', (req, res) => {
 
-    res.send('pagina de contato');
+    res.send('Página de contato');
 });
-app.get('/suporte', function (req, res){
+app.get('/Suporte', function (req, res){
 
-    res.send('pagina de suporte');
+    res.send('Página de suporte');
 });
 app.listen(porta, () =>{
-    console.log(`servidor iniciado na porta 3000: http://${hostname}:${porta}`);
+    console.log(`Servidor iniciado na porta 3000: http://${hostname}:${porta}`);
 });
