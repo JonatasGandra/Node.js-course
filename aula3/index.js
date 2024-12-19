1ac1b3a36bfc30104fe0e00a1bbc84bbc4203d1f
Jonatas Santos Gandra <jonatassgandra@gmail.com>
2024-12-18 21:56:50 -0300

    I created a series of instructions in the README to run the server with Express.

--- aula3/index.js
+++ aula3/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const porta = 3000;
-
+const hostname = '192.168.15.51'
 app.get('/', (req, res) => {
 
     res.send('pagina inicial');
@@ -14,10 +14,10 @@ app.get('/contato', (req, res) => {
 
     res.send('pagina de contato');
 });
-app.get('/suporte', (req, res) => {
+app.get('/suporte', function (req, res){
 
     res.send('pagina de suporte');
 });
 app.listen(porta, () =>{
-    console.log(`servidor iniciado na porta 8080: http://localhost:${porta}`);
+    console.log(`servidor iniciado na porta 3000: http://${hostname}:${porta}`);
 });
