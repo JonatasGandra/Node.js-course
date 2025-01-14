e4a92e9051ac3be78743b5e7ef5cf615fd0120b3
Jonatas Santos Gandra <jonatassgandra@gmail.com>
2025-01-14 17:42:09 -0300

    aprendi a usar o sequelize

---
+++ aula9/app.js
@@ -0,0 +1,59 @@
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize('sequelize_aula', 'root', 'Jsg1556!', {
+
+    host:'localhost',
+    dialect: 'mysql'
+
+})
+
+sequelize.authenticate().then(function(){
+
+    console.log("conexão realizada com sucesso!")
+}).catch(function(err){
+
+    console.log("Erro: conexão nao realizada com sucesso! Erro:" + err)
+})
+
+//DEFINE A MODELS
+const user = sequelize.define('users', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+})
+
+//criar a tabela com sequelize
+
+// se não houver a tabela users, entao criará a tabela
+user.sync()
+
+//se haver uma tabela chamada users, excluirá a tabela e criará outra
+//user.sync({force: true})
+
+//se houver uma diferença entre o que foi criado no node para o que ja esta no banco de dados, haverá edição da tabela no mysql
+//user.sync({alter: true})
+
+//Cadastrar registro no banco de dados (INSERT)
+
+user.create({
+    name: 'Jonatas',
+    email: 'jonatassgandra@gmail.com',
+    password: 'Jsg1556!'
+
+})
